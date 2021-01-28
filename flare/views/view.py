c64178952fbcbf807118ce67f49e9b39dbf14c6f
Stefan Kögl <sk@mausbrand.de>
2021-01-28 22:46:58 +0100

    * view needs more complex loaded state

--- flare/views/view.py
+++ flare/views/view.py
@@ -22,7 +22,7 @@ class View():
 			self.name = self.__class__.__name__.lower()
 
 		self.widgets = dictOfWidgets
-		self.loaded = False
+		self.loaded = {key: False for key in self.widgets.keys()}
 
 		flare.views.conf[ "views_state" ].register( "activeView", self )
 
@@ -33,43 +33,47 @@ class View():
 
 	def loadView( self ):
 		for target, widget in self.widgets.items():
-			logging.debug("loadView: %r, %r, %r", target, widget, conf[ "app" ])
 			try:
-				targetWidget = getattr( conf[ "app" ], target )
+				logging.debug("loadView: %r, %r, %r", target, widget, conf[ "app" ])
+				try:
+					targetWidget = getattr( conf[ "app" ], target )
+				except Exception as err:
+					logging.error( 'Target or conf["app"] %r not found!', target)
+					logging.exception(err)
+					ValueError( 'Target or conf["app"] %s not found!' % target)
+					return
+
+				logging.debug("before removeAllChildren: %r, %r, %r", target, widget, targetWidget)
+				targetWidget.removeAllChildren()
+				logging.debug("after removeAllCHildren")
+
+				if widget is None:
+					logging.debug("widget is None: %r, %r, %r", target, widget, targetWidget)
+					targetWidget.hide()
+				else:
+					if not self.loaded[target]:
+
+						# try:
+						self.wdgt = widget( self )
+						self.instancename = self.name
+						# except Exception as e:
+						#	print(e)
+						#	ValueError("ERROR: widget must have a view parameter")
+						#	wdgt = html5.Div("ERROR: widget must have a view parameter")
+						self.widgets.update( { target: self.wdgt } )
+						self.loaded[target] = True
+						logging.debug("created view widget?: %r, %r, %r, %r", self.wdgt, self.instancename, self.widgets, self.loaded)
+
+					if self.wdgt:
+						logging.debug("updateState viewfocused: %r", self.name)
+						self.wdgt.state.updateState("viewfocused", self.name)
+
+					logging.debug("before targetWidget show and appendContent")
+					targetWidget.show()
+					targetWidget.appendChild( self.widgets[ target ] )
 			except Exception as err:
-				logging.error( 'Target or conf["app"] %r not found!', target)
+				logging.error("something went wrong...")
 				logging.exception(err)
-				ValueError( 'Target or conf["app"] %s not found!' % target)
-				return
-
-			logging.debug("before removeAllCHildren: %r, %r, %r", target, widget, targetWidget)
-			targetWidget.removeAllChildren()
-			logging.debug("after removeAllCHildren")
-
-			if widget is None:
-				logging.debug("widget is None: %r, %r, %r", target, widget, targetWidget)
-				targetWidget.hide()
-			else:
-				if not self.loaded:
-
-					# try:
-					self.wdgt = widget( self )
-					self.instancename = self.name
-					# except Exception as e:
-					#	print(e)
-					#	ValueError("ERROR: widget must have a view parameter")
-					#	wdgt = html5.Div("ERROR: widget must have a view parameter")
-					self.widgets.update( { target: self.wdgt } )
-					self.loaded = True
-					logging.debug("created view widget?: %r, %r, %r, %r", self.wdgt, self.instancename, self.widgets, self.loaded)
-
-				if self.wdgt:
-					logging.debug("updateState viewfocused: %r", self.name)
-					self.wdgt.state.updateState("viewfocused", self.name)
-
-				logging.debug("before targetWidget show and appendContent")
-				targetWidget.show()
-				targetWidget.appendChild( self.widgets[ target ] )
 
 
 View.params = {}
