6237efdb7831f0a6cdb982dacd129260c4a0a313
Andreas H. Kelch <ak@mausbrand.de>
2021-02-18 09:43:25 +0100

    use markdown

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ extensions = []
 templates_path = ['_templates']
 
 # The suffix of source filenames.
-source_suffix = '.rst'
+source_suffix = '.md'
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
