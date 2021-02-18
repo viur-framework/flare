4d7798c902b196774d9ab810e04b66e62d42af71
Andreas H. Kelch <ak@mausbrand.de>
2021-02-18 09:19:22 +0100

    autoapi

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,8 @@ sys.path.insert(0, os.path.abspath('../../'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = ['autoapi.extension']
+autoapi_dirs = ['../flare']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
