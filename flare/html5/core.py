f349ea3bb6af81a474caf9f4b7d162efd6809642
Andreas H. Kelch <ak@mausbrand.de>
2021-02-22 09:45:33 +0100

    wtf

--- flare/html5/core.py
+++ flare/html5/core.py
@@ -2980,7 +2980,7 @@ def fromHTML(html: [str, HtmlAst], appendTo: Widget=None, bindTo: Widget=None, d
 		# Internal function for replacing {{ values["from"][4]["string"] + 1 }}...
 		ret = ""
 
-		while match := __reVarReplacer.search(txt):
+		while match = __reVarReplacer.search(txt):
 			ret += txt[:match.start()]
 			txt = txt[match.end():]
 
