e4c1b13314be4e1aade6f66e22912af7ad39602d
Jan Max Meyer <jm@mausbrand.de>
2021-02-22 09:21:17 +0100

    Improved addEventListener callback wrapping (#13)
    This change allows to use callback-functions either with no parameters or with the Event-object parameter and the widget

---
+++ examples/event-callback-parameters.py
@@ -0,0 +1,22 @@
+from flare import *
+
+html5.Head().appendChild("<style>.highlight { font-weight: bold; color: red; }</style>")
+
+class Test(html5.Ul):
+    def __init__(self):
+        super().__init__()
+        for i in range(5):
+            self.appendChild("<li @click='click{{ i if i <=2 else 2 }}'>Entry {{ i + 1 }}</li>", i=i)
+
+    def click0(self):
+        html5.Body().appendChild("Click0<br>")
+
+    def click1(self, event):
+        html5.Body().appendChild("Click1 %r<br>" % event)
+
+    def click2(self, event, widget):
+        html5.Body().appendChild("Click2 %r %r<br>" % (event, str(widget)))
+        widget.toggleClass("highlight")
+
+
+html5.Body().appendChild(Test())
