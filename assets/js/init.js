796b2d2eed34324a3a4b14366212ac7f579e5be5
Andreas <andreas.kelch@live.de>
2021-03-05 10:38:22 +0100

    init.js now can use files.zip per module

--- assets/js/init.js
+++ assets/js/init.js
@@ -4,6 +4,7 @@ emscripten virtual file system so that they can be called from Pyodide.
 
 Please see docs/setup.md in the section "HTML skeleton" for further information on how to use this script.
 */
+const SITE_PACKAGES = "/lib/python3.8/site-packages";
 
 class init {
 
@@ -35,7 +36,7 @@ class init {
 		});
 	}
 
-	loadSources(module, baseURL, files) {
+	loadPythonFilesAsSitePackage(module, baseURL, files) {
 		let promises = [];
 		let bar = null;
 		let info = null;
@@ -55,7 +56,7 @@ class init {
 					fetch(url, {}).then((response) => {
 						if (response.status === 200)
 							return response.text().then((code) => {
-								let path = ("/lib/python3.8/site-packages/" + module + "/" + file).split("/");
+								let path = (SITE_PACKAGES + "/" + module + "/" + file).split("/");
 								let lookup = "";
 
 								if (bar) {
@@ -96,44 +97,70 @@ class init {
 
 	fetchSources(modules) {
 		let promises = [];
+		window.pyodide.zipfiles = [];
 
 		for( let module of Object.keys(modules) )
 		{
 			promises.push(
 				new Promise((resolve, reject) => {
-					let mapfile = `${modules[module]["path"]}/files.json`;
-					fetch(mapfile, {}).then((response) => {
-						if (response.status === 200) {
-							response.text().then((list) => {
-								let files = [];
+				  // First try to download zip-file
+				  let zipurl = `${modules[module]["path"]}/files.zip`;
+				  fetch(zipurl, {}).then((response) => {
+				    if (response.status === 200) {
+				      return response.blob().then((blob) => {
+                let zipfile = "/" + module + ".zip";
 
-								try {
-									files = JSON.parse(list);
-								}
-								catch (e) {
-									if( modules[module]["optional"] ) {
-										console.info(`Optional module ${module} wasn't found`);
-										return resolve();
-									}
-									else {
-										console.error(`Unable to parse ${mapfile} properly, check for correct config of ${module}`);
-										return reject();
-									}
-								}
+                //window.pyodide._module.FS.writeFile(zipfile, content);
+                //window.pyodide._module.FS.createPreloadedFile("/", module + ".zip", zipurl, true, false);
+                blob.arrayBuffer().then(buffer => {
+                  buffer = new Uint8Array(buffer);
 
-								this.loadSources(module, modules[module]["path"], files).then(() => {
-									resolve();
-								})
-							})
-						} else {
-							if( modules[module]["optional"] ) {
-								console.info(`Optional module ${module} wasn't found`);
-								return resolve();
-							}
+                  let stream = window.pyodide._module.FS.open(zipfile, "w+");
+                  window.pyodide._module.FS.write(stream, buffer, 0, buffer.length, 0);
+                  window.pyodide._module.FS.close(stream);
 
-							reject();
-						}
-					})
+                  window.pyodide.zipfiles.push(zipfile);
+                  console.debug(`fetched ${zipfile}`);
+                  resolve();
+                });
+              });
+            } else {
+              let mapfile = `${modules[module]["path"]}/files.json`;
+              fetch(mapfile, {}).then((response) => {
+                if (response.status === 200) {
+                  response.text().then((list) => {
+                    let files = [];
+
+                    try {
+                      files = JSON.parse(list);
+                    }
+                    catch (e) {
+                      if( modules[module]["optional"] ) {
+                        console.info(`Optional module ${module} wasn't found`);
+                        return resolve();
+                      }
+                      else {
+                        console.error(`Unable to parse ${mapfile} properly, check for correct config of ${module}`);
+                        return reject();
+                      }
+                    }
+
+                    this.loadPythonFilesAsSitePackage(module, modules[module]["path"], files).then(() => {
+                      resolve();
+                    })
+                  })
+                } else {
+                  if( modules[module]["optional"] ) {
+                    console.info(`Optional module ${module} wasn't found`);
+                    return resolve();
+                  }
+
+                  reject();
+                }
+              });
+            }
+          });
+
 				}));
 		}
 
@@ -144,12 +171,21 @@ class init {
 			}catch (e) {}
 
 			for( let module of Object.keys(modules) ) {
-				window.pyodide.loadedPackages[module] = "default channel";
+			  window.pyodide.loadedPackages[module] = "default channel";
 			}
 
 			window.pyodide.runPython(
-				'import importlib as _importlib\n' +
-				'_importlib.invalidate_caches()\n'
+			  // language=Python
+				`
+import sys
+import importlib as _importlib
+from js import window
+
+for zipfile in window.pyodide.zipfiles:
+    sys.path.insert(0, zipfile)
+
+_importlib.invalidate_caches()
+        `
 			);
 
 
