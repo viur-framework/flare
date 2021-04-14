c3a7cb7139a0277ee81a18fa890287f44728d2e2
Andreas H. Kelch <ak@mausbrand.de>
2021-04-14 12:57:06 +0200

    feat: added intersectionObserver wrapper

---
+++ flare/intersectionObserver.py
@@ -0,0 +1,36 @@
+from flare import conf
+from js import IntersectionObserver as js_intersectionObserver
+
+class IntersectionObserver:
+	'''
+		Usage:
+			myObserver = IntersectionObserver(myChangeFunction)
+			myObserver.observe(aWidget)
+	'''
+	jsObserver = None
+	observableWidgets = []
+
+	def __init__(self, callback, rootWidget=None, rootMargin="0px", threshold=0.2):
+		observerOptions = {
+			"root": conf["app"].element if not rootWidget else rootWidget.element,
+			"rootMargin": rootMargin,
+			"threshold": threshold
+		}
+
+		self.jsObserver = js_intersectionObserver.new(callback, observerOptions)
+
+	def observe(self, widget):
+		self.observableWidgets.append(widget)
+		self.jsObserver.observe(widget.element)
+
+	def unobserve(self,widget):
+		self.observableWidgets.remove(widget)
+		self.jsObserver.unobserve(widget.element)
+
+
+
+
+
+
+
+
