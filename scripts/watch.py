88ca58d921f6dc8281ead63741c80cdd84ea0072
Andreas H. Kelch <ak@mausbrand.de>
2021-04-14 08:43:33 +0200

    feat: added a watcher for better development

---
+++ scripts/watch.py
@@ -0,0 +1,45 @@
+import os, pathlib, shutil, argparse
+from watchgod import watch, PythonWatcher
+from watchgod.watcher import Change
+
+PATHBLACKLIST = ["/docs/","/examples/","/bin/","/scripts/","/test/", "/assets/"]
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='watch a flare application and update py files on change')
+	parser.add_argument('source', help='source path relativ to projectpath')
+	parser.add_argument('target', help='target path relativ to projectpath')
+	parser.add_argument("-n", "--name", help='application name', default="vi")
+	args = parser.parse_args()
+
+	sourcePath = args.source.strip("/") #'sources/viur-vi/vi'
+	targetPath = args.target.strip("/") #'deploy/vi'
+	applicationName = args.name
+
+
+	PROJECT_WORKSPACE = os.environ.get("PROJECT_WORKSPACE", ".")
+	os.chdir(PROJECT_WORKSPACE)
+
+	# build on start with assets
+	print("initial build... please wait")
+	flareFolder = str(pathlib.Path(__file__).absolute()).split("/scripts")[0]
+	os.system(f'python3 {flareFolder}/scripts/flare.py -t {PROJECT_WORKSPACE}/{targetPath} -s {PROJECT_WORKSPACE}/{sourcePath} -n {applicationName}')
+
+	# only watch py files
+	print(f"../watching")
+	for changes in watch(os.path.join(PROJECT_WORKSPACE,sourcePath), watcher_cls=PythonWatcher):
+		changes = list(changes)
+
+		#dont copy py files from blacklisten folders
+		if any(map(changes[0][1].__contains__,PATHBLACKLIST)):
+			continue
+
+		if changes[0][0] in [Change.added, Change.deleted]:
+			print("please update files.json")
+		else:
+			print(f"update {changes[0][1]}")
+
+		filepath = changes[0][1].replace(str(os.path.join(PROJECT_WORKSPACE,sourcePath)+"/"),"")
+
+		#copy changed file
+		shutil.copy(changes[0][1], os.path.join(PROJECT_WORKSPACE,targetPath,filepath))
+
