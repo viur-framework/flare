5a1d97d5f66f8caaff7dddd74fa3cbde8f66dcaa
Achim Schumacher <as@mausbrand.de>
2021-06-24 13:53:40 +0200

    Replaced svg.py by latest version of html5's pyodide-svg branch

--- flare/html5/svg.py
+++ flare/html5/svg.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 """SVG abstraction layer integrations for HTML5."""
 
 from . import core as html5
@@ -93,11 +95,37 @@ class _attrSvgDimensions(object):
 
 
 class _attrSvgPoints(object):
+    """
+    The SVGAnimatedPoints interface is used to reflect a ‘points’ attribute on a ‘polygon’ or ‘polyline’ element.
+    It is mixed in to the SVGPolygonElement and SVGPolylineElement interfaces
+    """
+
     def _getPoints(self):
+        """
+        returns SVGPointList object, see www.w3.org/TR/SVG2/shapes.html
+
+        interface SVGPointList {
+
+            readonly attribute unsigned long length;
+            readonly attribute unsigned long numberOfItems;
+
+            void clear();
+            DOMPoint initialize(DOMPoint newItem);
+            getter DOMPoint getItem(unsigned long index);
+            DOMPoint insertItemBefore(DOMPoint newItem, unsigned long index);
+            DOMPoint replaceItem(DOMPoint newItem, unsigned long index);
+            DOMPoint removeItem(unsigned long index);
+            DOMPoint appendItem(DOMPoint newItem);
+            setter void (unsigned long index, DOMPoint newItem);
+        };
+        """
         return self.element.points
 
     def _setPoints(self, val):
         self.element.setAttribute("points", val)
+
+
+class _attrSvgTwoPoints(object):
 
     def _getX1(self):
         return self.element.x1
@@ -141,17 +169,75 @@ class _attrSvgXlink(object):
 
 
 class _attrSvgStyles(object):
+
+    def __init__(self, *args, **kwargs):
+        if 'fill' in kwargs:
+            self._setFill(kwargs.pop('fill'))
+        if 'fillopacity' in kwargs:
+            self._setFillopacity(kwargs.pop('fillopacity'))
+        if 'stroke' in kwargs:
+            self._setStroke(kwargs.pop('stroke'))
+        if 'strokeopacity' in kwargs:
+            self._setStrokeopacity(kwargs.pop('strokeopacity'))
+        if 'strokewidth' in kwargs:
+            self._setStrokewidth(kwargs.pop('strokewidth'))
+        if 'strokelinejoin' in kwargs:
+            self._setStrokelinejoin(kwargs.pop('strokelinejoin'))
+        if 'strokelinecap' in kwargs:
+            self._setStrokelinecap(kwargs.pop('strokelinecap'))
+        if 'strokedasharray' in kwargs:
+            self._setStrokedasharray(kwargs.pop('strokedasharray'))
+        super().__init__()
+
     def _getFill(self):
         return self.element.fill
 
     def _setFill(self, val):
+        '''Set the fill color'''
         self.element.setAttribute("fill", val)
+
+    def _getFillopacity(self):
+        return self.element.getAttribute("fill-opacity")
+
+    def _setFillopacity(self, val):
+        self.element.setAttribute("fill-opacity", val)
 
     def _getStroke(self):
         return self.element.stroke
 
     def _setStroke(self, val):
+        '''Set the stroke color'''
         self.element.setAttribute("stroke", val)
+
+    def _getStrokeopacity(self):
+        return self.element.getAttribute("stroke-opacity")
+
+    def _setStrokeopacity(self, val):
+        self.element.setAttribute("stroke-opacity", val)
+
+    def _getStrokewidth(self):
+        return self.element.getAttribute("stroke-width")
+
+    def _setStrokewidth(self, val):
+        self.element.setAttribute("stroke-width", val)
+
+    def _getStrokelinecap(self):
+        return self.element.getAttribute("stroke-linecap")
+
+    def _setStrokelinecap(self, val):
+        self.element.setAttribute("stroke-linecap", val)
+
+    def _getStrokelinejoin(self):
+        return self.element.getAttribute("stroke-linejoin")
+
+    def _setStrokelinejoin(self, val):
+        self.element.setAttribute("stroke-linejoin", val)
+
+    def _getStrokedasharray(self):
+        return self.element.getAttribute("stroke-dasharray")
+
+    def _setStrokedasharray(self, val):
+        self.element.setAttribute("stroke-dasharray", val)
 
 
 ########################################################################################################################
@@ -166,6 +252,17 @@ class SvgWidget(html5.Widget):
 
 @html5.tag
 class Svg(SvgWidget, _attrSvgViewBox, _attrSvgDimensions, _attrSvgTransform):
+    """
+    From DOM's SvgSvgElement, following methods are inherited:
+    - createSVGNumber
+    - createSVGPoint
+    - createSVGLength
+    - createSVGAngle
+    - createSVGMatrix
+    - createSVGRect
+    - createSVGTransform
+
+    """
     _tagName = "svg"
 
     def _getVersion(self):
@@ -180,20 +277,53 @@ class Svg(SvgWidget, _attrSvgViewBox, _attrSvgDimensions, _attrSvgTransform):
     def _setXmlns(self, val):
         self.element.setAttribute("xmlns", val)
 
+    def createSVGNumber(self):
+        return self.element.createSVGNumber()
+
+    def createSVGPoint(self):
+        return self.element.createSVGPoint()
+
+    def createSVGLength(self):
+        return self.element.createSVGLength()
+
+    def createSVGAngle(self):
+        return self.element.createSVGAngle()
+
+    def createSVGMatrix(self):
+        return self.element.createSVGMatrix()
+
+    def createSVGRect(self):
+        return self.element.createSVGRect()
+
+    def createSVGTransform(self):
+        return self.element.createSVGTransform()
+
 
 @html5.tag
-class SvgCircle(SvgWidget, _attrSvgTransform, _attrSvgDimensions):
+class SvgCircle(SvgWidget, _attrSvgTransform, _attrSvgDimensions, _attrSvgStyles):
     _tagName = "circle"
 
+    def __init__(self, *args, **kwargs):
+        SvgWidget.__init__(self, *args, **kwargs)
+        _attrSvgStyles.__init__(self, *args, **kwargs)
+
 
 @html5.tag
-class SvgEllipse(SvgWidget, _attrSvgTransform, _attrSvgDimensions):
+class SvgEllipse(SvgWidget, _attrSvgTransform, _attrSvgDimensions, _attrSvgStyles):
     _tagName = "ellipse"
+
+    def __init__(self, *args, **kwargs):
+        SvgWidget.__init__(self, *args, **kwargs)
+        _attrSvgStyles.__init__(self, *args, **kwargs)
 
 
 @html5.tag
 class SvgG(SvgWidget, _attrSvgTransform, _attrSvgStyles):
     _tagName = "g"
+
+    def __init__(self, *args, **kwargs):
+        SvgWidget.__init__(self, *args, **kwargs)
+        _attrSvgStyles.__init__(self, *args, **kwargs)
 
     def _getSvgTransform(self):
         return self.element.transform
@@ -203,23 +333,29 @@ class SvgG(SvgWidget, _attrSvgTransform, _attrSvgStyles):
 
 
 @html5.tag
-class SvgImage(
-    SvgWidget, _attrSvgViewBox, _attrSvgDimensions, _attrSvgTransform, _attrSvgXlink
-):
+class SvgImage(SvgWidget, _attrSvgViewBox, _attrSvgDimensions, _attrSvgTransform, _attrSvgXlink):
     _tagName = "image"
 
 
 @html5.tag
-class SvgLine(SvgWidget, _attrSvgTransform, _attrSvgPoints):
+class SvgLine(SvgWidget, _attrSvgTransform, _attrSvgTwoPoints, _attrSvgStyles):
     _tagName = "line"
+
+    def __init__(self, *args, **kwargs):
+        SvgWidget.__init__(self, *args, **kwargs)
+        _attrSvgStyles.__init__(self, *args, **kwargs)
 
 
 @html5.tag
-class SvgPath(SvgWidget, _attrSvgTransform):
+class SvgPath(SvgWidget, _attrSvgTransform, _attrSvgStyles):
     _tagName = "path"
 
+    def __init__(self, *args, **kwargs):
+        SvgWidget.__init__(self, *args, **kwargs)
+        _attrSvgStyles.__init__(self, *args, **kwargs)
+
     def _getD(self):
-        return self.element.d
+        return self.element.getAttribute("d")
 
     def _setD(self, val):
         self.element.setAttribute("d", val)
@@ -232,63 +368,111 @@ class SvgPath(SvgWidget, _attrSvgTransform):
 
 
 @html5.tag
-class SvgPolygon(SvgWidget, _attrSvgTransform, _attrSvgPoints):
+class SvgPolygon(SvgWidget, _attrSvgTransform, _attrSvgPoints, _attrSvgStyles):
+    """
+    SvgWidget representing a "polygon" element in an SVG.
+    """
     _tagName = "polygon"
+
+    def __init__(self, *args, **kwargs):
+        SvgWidget.__init__(self, *args, **kwargs)
+        _attrSvgStyles.__init__(self, *args, **kwargs)
 
 
 @html5.tag
-class SvgPolyline(SvgWidget, _attrSvgTransform, _attrSvgPoints):
+class SvgPolyline(SvgWidget, _attrSvgTransform, _attrSvgPoints, _attrSvgStyles):
     _tagName = "polyline"
+
+    def __init__(self, *args, **kwargs):
+        SvgWidget.__init__(self, *args, **kwargs)
+        _attrSvgStyles.__init__(self, *args, **kwargs)
 
 
 @html5.tag
 class SvgRect(SvgWidget, _attrSvgDimensions, _attrSvgTransform, _attrSvgStyles):
     _tagName = "rect"
 
+    def __init__(self, *args, **kwargs):
+        SvgWidget.__init__(self, *args, **kwargs)
+        _attrSvgStyles.__init__(self, *args, **kwargs)
+
 
 @html5.tag
 class SvgText(SvgWidget, _attrSvgDimensions, _attrSvgTransform, _attrSvgStyles):
     _tagName = "text"
 
+    def __init__(self, *args, **kwargs):
+        txt = kwargs.pop('text', "")
+        SvgWidget.__init__(self, *args, **kwargs)
+        _attrSvgStyles.__init__(self, *args, **kwargs)
+        self._setText(txt)
+
+    def _getTextanchor(self):
+        return self.element.getAttribute("text-anchor")
+
+    def _setTextanchor(self, val):
+        self.element.setAttribute("text-anchor", val)
+
+    def _getFontfamily(self):
+        return self.element.getAttribute("font-family")
+
+    def _setFontfamily(self, val):
+        self.element.setAttribute("font-family", val)
+
+    def _getFontsize(self):
+        return self.element.getAttribute("font-size")
+
+    def _setFontsize(self, val):
+        self.element.setAttribute("font-size", val)
+
+    def _getText(self):
+        return self.element.textContent
+
+    def _setText(self, txt):
+        self.element.textContent = txt
+
+    def __str__(self):
+        return self.element.textContent
+
 
 """ later...
 
 class SvgDefs(SvgWidget):
-	_tagName = "defs"
+    _tagName = "defs"
 
 
 class SvgClipPath(SvgWidget):
-	_tagName = "clippath"
+    _tagName = "clippath"
 
 
 class SvgLinearGradient(SvgWidget, _attrSvgPoints):
-	_tagName = "lineargradient"
+    _tagName = "lineargradient"
 
-	def _setGradientunits(self, value):
-		self.element.gradientUnits = value
+    def _setGradientunits(self, value):
+        self.element.gradientUnits = value
 
-	def _getGradientunits(self):
-		return self.element.gradientUnits
+    def _getGradientunits(self):
+        return self.element.gradientUnits
 
-	def _setGradienttransform(self, value):
-		self.element.gradientTransform = value
+    def _setGradienttransform(self, value):
+        self.element.gradientTransform = value
 
-	def _getGradienttransform(self):
-		return self.element.gradientTransform
+    def _getGradienttransform(self):
+        return self.element.gradientTransform
 
 
 class SvgStop(SvgWidget):
-	_tagName = "stop"
+    _tagName = "stop"
 
-	def _setOffset(self, value):
-		self.element.offset = value
+    def _setOffset(self, value):
+        self.element.offset = value
 
-	def _getOffset(self):
-		return self.element.offset
+    def _getOffset(self):
+        return self.element.offset
 
-	def _setStopcolor(self, value):
-		self.element.offset = value
+    def _setStopcolor(self, value):
+        self.element.offset = value
 
-	def _getStopcolor(self):
-		return self.element.offset
+    def _getStopcolor(self):
+        return self.element.offset
 """
