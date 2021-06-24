0e9add82b063c2f568238e007c7ec4028cc72186
Achim Schumacher <as@mausbrand.de>
2021-06-24 14:04:10 +0200

    Removed inline style

--- flare/popup.py
+++ flare/popup.py
@@ -19,7 +19,7 @@ class Popup(html5.Div):
         # language=HTML
         super().__init__(
             """
-            <div class="box" [name]="popupBox" style="height: 100%">
+            <div class="box" [name]="popupBox" >
                 <div class="box-head" [name]="popupHead">
                     <div class="item" [name]="popupHeadItem">
                         <div class="item-image">
@@ -36,7 +36,7 @@ class Popup(html5.Div):
                             class="item-action btn--transparent btn--icon">
                     </div>
                 </div>
-                <div class="box-body box--content" [name]="popupBody" style="height: 100%"></div>
+                <div class="box-body box--content" [name]="popupBody" ></div>
                 <div class="box-foot box--content bar" [name]="popupFoot"></div>
             </div>
             """,
