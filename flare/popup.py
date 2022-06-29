d9a2a8045892f5cf557387c1faf9a598d9d51579
Grashalmbeisser <57599830+Grashalmbeisser@users.noreply.github.com>
2022-06-29 11:09:03 +0200

    Updated popup.py (#56)
    * Updated popup.py
    
    Fixed bug not showing a Scrollbar on wide PopUps
    
    * Added fixme comment to PR's change
    
    Co-authored-by: Jan Max Meyer <jmm@phorward.de>

--- flare/popup.py
+++ flare/popup.py
@@ -20,7 +20,7 @@ class Popup(html5.Div):
         # language=HTML
         super().__init__(
             """
-            <div class="box" [name]="popupBox" style="height: 100%">
+            <div class="box" [name]="popupBox" style="height: 100%; overflow: auto"> <!-- fixme: Remove inline style here! -->
                 <div class="box-head" [name]="popupHead">
                     <div class="item" [name]="popupHeadItem">
                         <div class="item-image">
