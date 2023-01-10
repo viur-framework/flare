1e07a3c22be0a8917dfa7e5ae2f8c9217a3d0b23
Jan Max Meyer <jmm@phorward.de>
2023-01-10 12:52:44 +0100

    Try to obtain defaultvalue from bone structure

--- flare/viur/forms.py
+++ flare/viur/forms.py
@@ -91,7 +91,7 @@ class ViurForm(html5.Form):
             if key in self.ignore or (self.visible and key not in self.visible):
                 continue
 
-            bone_field = ViurFormBone(key, self)
+            bone_field = ViurFormBone(key, self, defaultvalue=bone.get("defaultvalue"))
             bone_field.onAttach()  # needed for value loading!  # fixme should be solved differently.
 
             # Hide invisible fields or fields with conditional flagging
